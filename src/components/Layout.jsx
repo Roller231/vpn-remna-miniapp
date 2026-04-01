d6c69a953fca96fda592e63145a34773eb1a97c5
Ruslan <lelyimalikrusik12301@gmail.com>
2026-04-01 20:44:37 +0300

    ввв

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,8 +1,21 @@
+import { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
 import BottomNav from './BottomNav/index'
 import AnimatedBackground from './AnimatedBackground'
 import './Layout.css'
 
 const Layout = ({ children }) => {
+  const location = useLocation()
+
+  useEffect(() => {
+    // Scroll to top when route changes
+    const mainContent = document.querySelector('.main-content')
+    if (mainContent) {
+      mainContent.scrollTop = 0
+    }
+    window.scrollTo(0, 0)
+  }, [location.pathname])
+
   return (
     <div className="layout">
       <AnimatedBackground />
