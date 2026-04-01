54ed1d0c2c9b71c9fa97aa6c5d4ccad89a30ed21
Ruslan <lelyimalikrusik12301@gmail.com>
2026-04-01 20:01:48 +0300

    ы

--- src/components/AnimatedBackground/index.jsx
+++ src/components/AnimatedBackground/index.jsx
@@ -1,32 +1,8 @@
-import { useEffect, useRef } from 'react'
-import { useLocation } from 'react-router-dom'
 import './AnimatedBackground.css'
 
 const AnimatedBackground = () => {
-  const location = useLocation()
-  const containerRef = useRef(null)
-  const animationSpeedRef = useRef(1)
-
-  useEffect(() => {
-    animationSpeedRef.current = 3
-    const timer = setTimeout(() => {
-      animationSpeedRef.current = 1
-    }, 800)
-
-    if (containerRef.current) {
-      containerRef.current.style.setProperty('--animation-speed', '3')
-      setTimeout(() => {
-        if (containerRef.current) {
-          containerRef.current.style.setProperty('--animation-speed', '1')
-        }
-      }, 800)
-    }
-
-    return () => clearTimeout(timer)
-  }, [location.pathname])
-
   return (
-    <div className="animated-background" ref={containerRef}>
+    <div className="animated-background">
       <div className="gradient-blob blob-1"></div>
       <div className="gradient-blob blob-2"></div>
       <div className="gradient-blob blob-3"></div>
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,9 +1,11 @@
 import BottomNav from './BottomNav/index'
+import AnimatedBackground from './AnimatedBackground'
 import './Layout.css'
 
 const Layout = ({ children }) => {
   return (
     <div className="layout">
+      <AnimatedBackground />
       <main className="main-content">
         {children}
       </main>
